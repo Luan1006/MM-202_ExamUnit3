ef030248e34cec4ae1f39e72154cfcf412a9ab3e
Luan1006 <thenoobsisbest@gmail.com>
2024-03-19 20:14:40 +0100

    Created test for SquareNumber

--- Tests/FunctionsAreAPoppingTests.cs
+++ Tests/FunctionsAreAPoppingTests.cs
@@ -2,8 +2,20 @@ using Xunit;
 
 namespace MM202ExamUnit3Tests
 {
-    class FunctionsAreAPoppingTests
+    public class FunctionsAreAPoppingTests
     {
+        [Fact]
+        public void SquareNumber_ReturnsSquaredNumber_WhenNumberIsGiven()
+        {
+            // Arrange
+            int number = 4;
+            int expected = 16;
 
+            // Act
+            int result = SquareNumber(number);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
