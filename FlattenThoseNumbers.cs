3323b5aab5c43d71d333fd52e1ba6fa83a5d0d66
Luan1006 <thenoobsisbest@gmail.com>
2024-03-20 17:48:13 +0100

    Created FlattenArray method

--- FlattenThoseNumbers.cs
+++ FlattenThoseNumbers.cs
@@ -2,6 +2,23 @@ namespace MM202ExamUnit3
 {
     public class FlattenThoseNumbers
     {
-        
+        public static int[] FlattenArray(object[] arr)
+        {
+            List<int> result = new List<int>();
+
+            foreach (var item in arr)
+            {
+                if (item is int)
+                {
+                    result.Add((int)item);
+                }
+                else if (item is object[])
+                {
+                    result.AddRange(FlattenArray((object[])item));
+                }
+            }
+
+            return result.ToArray();
+        }
     }
 }
