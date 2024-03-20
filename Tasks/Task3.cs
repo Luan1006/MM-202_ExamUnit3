090308489ebb9566ec67398dbacdbf8a46259572
Luan1006 <thenoobsisbest@gmail.com>
2024-03-20 21:36:53 +0100

    Created Nodes and TreeInfo

--- Tasks/Task3.cs
+++ Tasks/Task3.cs
@@ -3,5 +3,19 @@ namespace MM202ExamUnit3
     public class Task3
     {
 
+        // setting it up as a binary tree
+        public class Node
+        {
+            public int Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+
+        public class TreeInfo
+        {
+            public int Sum { get; set; }
+            public int Depth { get; set; }
+            public int Count { get; set; }
+        }
     }
 }
