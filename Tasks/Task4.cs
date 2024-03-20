27c5fcd0a8b33700fd7fbed108887622dfc4825e
Luan1006 <thenoobsisbest@gmail.com>
2024-03-21 00:47:22 +0100

    Created book class and updated .gitignore

--- Tasks/Task4.cs
+++ Tasks/Task4.cs
@@ -1,5 +1,12 @@
 namespace MM202ExamUnit3
 {
+    public class Book
+    {
+        public string title { get; set; }
+        public string publication_year { get; set; }
+        public string author { get; set; }
+        public string isbn { get; set; }
+    }
     public class Task4
     {
         
