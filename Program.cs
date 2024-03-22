a0d12f27d73656049a743440d11083b2caf13a93
Luan1006 <thenoobsisbest@gmail.com>
2024-03-22 14:23:45 +0100

    Removed some magic strings from Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using MM202ExamUnit3.Utils;
 using static MM202ExamUnit3.Utils.Print;
+using static MM202ExamUnit3.Utils.Constants;
 
 namespace MM202ExamUnit3
 {
@@ -16,21 +17,21 @@ namespace MM202ExamUnit3
         {
             Console.Clear();
 
-            Console.WriteLine("Welcome to the Jagged Array Flattener!\n");
+            Console.WriteLine(WelcomeToJaggedArrayFlattener);
 
             try
             {
-                Console.WriteLine("Calling the API...");
+                Console.WriteLine(CallAPI);
 
                 HttpResponseMessage response = await apiService.GetApiResponse(Constants.JaggedAPIURL);
                 string responseBody = await response.Content.ReadAsStringAsync();
                 JsonDocument doc = apiService.ParseJsonString(responseBody);
 
-                Console.WriteLine($"Jagged array: {doc.RootElement}");
+                Console.WriteLine(JaggedArray, doc.RootElement);
 
                 int[] flattenedArray = arrayProcessor.FlattenJsonArray(doc);
                 string arrayToWrite = string.Join(", ", flattenedArray);
-                Console.WriteLine($"Flattened array: [{arrayToWrite}]\n");
+                Console.WriteLine(FlattenedArray, arrayToWrite);
             }
             catch (HttpRequestException e)
             {
@@ -39,43 +40,43 @@ namespace MM202ExamUnit3
 
             try
             {
-                Console.WriteLine("Reading the JSON file...");
+                Console.WriteLine(ReadJSON);
 
-                jsonFilePath = Path.Combine("ExampleFiles", "arrays.json");
+                jsonFilePath = Path.Combine(ExampleFilesDirectory, ArraysJsonExampleFile);
                 jsonContent = await File.ReadAllTextAsync(jsonFilePath);
                 JsonDocument doc = JsonDocument.Parse(jsonContent);
 
-                Console.WriteLine($"Jagged array:\n{doc.RootElement}");
+                Console.WriteLine(JaggedArray, doc.RootElement);
 
                 int[] flattenedArray = arrayProcessor.FlattenJsonArray(doc);
                 string arrayToWrite = string.Join(", ", flattenedArray);
-                Console.WriteLine($"Flattened array: [{arrayToWrite}]");
+                Console.WriteLine(FlattenedArray, arrayToWrite);
             }
             catch (Exception e)
             {
                 PrintErrorMessage(e.Message);
             }
 
-            Console.WriteLine("\nWelcome to the Binary Tree Traverser!\n");
+            Console.WriteLine(WelcomeToTheBinaryTreeTraverser);
 
             try
             {
-                Console.WriteLine("Calling the API...");
+                Console.WriteLine(CallAPI);
 
                 HttpResponseMessage response = await apiService.GetApiResponse(Constants.BSTAPIURL);
                 string responseBody = await response.Content.ReadAsStringAsync();
 
                 Task3 task3 = new Task3();
 
-                Console.WriteLine($"Binary tree: {responseBody}");
+                Console.WriteLine(BinaryTree, responseBody);
 
                 Task3.Node root = JsonSerializer.Deserialize<Task3.Node>(responseBody);
 
                 Task3.TreeInfo treeInfo = task3.Traverse(root);
 
-                Console.WriteLine($"Sum = {treeInfo.Sum}");
-                Console.WriteLine($"Deepest level = {treeInfo.Depth}");
-                Console.WriteLine($"Nodes = {treeInfo.Count}");
+                Console.WriteLine(Sum, treeInfo.Sum);
+                Console.WriteLine(DeepestLevel, treeInfo.Depth);
+                Console.WriteLine(Nodes, treeInfo.Count);
             }
             catch (HttpRequestException e)
             {
@@ -84,30 +85,30 @@ namespace MM202ExamUnit3
 
             try
             {
-                Console.WriteLine("Reading the JSON file...");
+                Console.WriteLine(ReadJSON);
 
-                jsonFilePath = Path.Combine("ExampleFiles", "nodes.json");
+                jsonFilePath = Path.Combine(ExampleFilesDirectory, NodesJsonExampleFile);
                 jsonContent = await File.ReadAllTextAsync(jsonFilePath);
 
                 Task3 task3 = new Task3();
 
-                Console.WriteLine($"Binary tree: {jsonContent}");
+                Console.WriteLine(BinaryTree, jsonContent);
 
                 Task3.Node root = JsonSerializer.Deserialize<Task3.Node>(jsonContent);
 
                 Task3.TreeInfo treeInfo = task3.Traverse(root);
 
-                Console.WriteLine($"Sum = {treeInfo.Sum}");
-                Console.WriteLine($"Deepest level = {treeInfo.Depth}");
-                Console.WriteLine($"Nodes = {treeInfo.Count}");
+                Console.WriteLine(Sum, treeInfo.Sum);
+                Console.WriteLine(DeepestLevel, treeInfo.Depth);
+                Console.WriteLine(Nodes, treeInfo.Count);
             }
             catch (Exception e)
             {
                 PrintErrorMessage(e.Message);
             }
 
-            Console.WriteLine("\nWelcome to the Book List Processor!\n");
-            jsonFilePath = Path.Combine("ExampleFiles", "books.json");
+            Console.WriteLine(WelcomeToTheBookListProcessor);
+            jsonFilePath = Path.Combine(ExampleFilesDirectory, BooksJsonExampleFile);
             jsonContent = await File.ReadAllTextAsync(jsonFilePath);
 
             Task4 task4 = new Task4(jsonContent);
@@ -116,10 +117,10 @@ namespace MM202ExamUnit3
 
             foreach (IGrouping<string, Book> group in lastNameGroups)
             {
-                Console.WriteLine($"Author Last Name: {group.Key}");
+                Console.WriteLine(AuthorLastName + group.Key);
                 foreach (Book book in group)
                 {
-                    Console.WriteLine($"Title: {book.title}");
+                    Console.WriteLine(Title + book.title);
                 }
                 Console.WriteLine();
             }
--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -5,5 +5,22 @@ namespace MM202ExamUnit3.Utils
         public const double PI = 3.141592653589793;
         public const string JaggedAPIURL = "https://crismo-turquoisejaguar.web.val.run/arrayI";
         public const string BSTAPIURL = "https://crismo-turquoisejaguar.web.val.run/treeI";
+        public const string WelcomeToJaggedArrayFlattener = "Welcome to the Jagged Array Flattener!\n";
+        public const string CallAPI = "Calling the API...";
+        public const string JaggedArray = "Jagged array: {0}";
+        public const string FlattenedArray = "Flattened array: {0}";
+        public const string ReadJSON = "Reading the JSON file...";
+        public const string ExampleFilesDirectory = "ExampleFiles";
+        public const string ArraysJsonExampleFile = "arrays.json";
+        public const string WelcomeToTheBinaryTreeTraverser= "Welcome to the Binary Tree Traverser!\n";
+        public const string BinaryTree= "Binary tree: {0}";
+        public const string Sum = "Sum = {0}";
+        public const string DeepestLevel = "Deepest level = {0}";
+        public const string Nodes = "Nodes = {0}";
+        public const string NodesJsonExampleFile = "nodes.json";
+        public const string WelcomeToTheBookListProcessor = "Welcome to the Book List Processor!\n";
+        public const string BooksJsonExampleFile = "books.json";
+        public const string AuthorLastName= "Author Last Name: ";
+        public const string Title = "Title: ";
     }
 }
